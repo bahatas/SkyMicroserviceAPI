928ed6a3c8a9f8eb56341fdd476230cd35e7bef0
BAHATTIN TASTAN <59250233+bahatas@users.noreply.github.com>
2022-06-11 23:43:00 +0100

    sky service initial commit

---
+++ sky-service/src/main/java/com/sky/skyservice/SkyServiceApplication.java
@@ -0,0 +1,13 @@
+package com.sky.skyservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkyServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SkyServiceApplication.class, args);
+    }
+
+}
---
+++ sky-service/src/test/java/com/sky/skyservice/SkyServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sky.skyservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkyServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
